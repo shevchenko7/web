0879c907a841feb42b7e27b7ca8b430da1a1e41a
황형훈 <messi09@wemakeprice.com>
2022-01-27 17:50:16 +0900

    spring boot add

---
+++ src/main/java/com/sample/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.sample.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/sample")
+public class SampleController {
+
+    @GetMapping("/main")
+    public String main() {
+        return "sample main";
+    }
+}
